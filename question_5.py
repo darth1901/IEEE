93dfc6d9f494f86bb9a6a1905c5c0e285c5073f5
darth1901 <parthd1901@gmail.com>
2025-09-05 17:24:56 +0530

    Q5

---
+++ question_5.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+  # 1. Define the number of random numbers to generate.
+  num_points = 1000
+
+  #generating the random numbers
+  #a normal distribution has a mean of 0 and a standard deviation of 1.
+  # np.random.randn() is a convenient function for this.
+  random_values = np.random.randn(num_points)
+
+
+  x_axis = np.arange(num_points)
+
+
+
+  plt.figure(figsize=(12, 7))
+  plt.scatter(x_axis, random_values, alpha=0.7, edgecolors='b', s=30)
+
+ 
+  plt.title('Scatter Plot of 1000 Random Numbers from a Normal Distribution', fontsize=16)
+  plt.xlabel('Index', fontsize=12)
+  plt.ylabel('Random Value', fontsize=12)
+
+
+  plt.grid(True, linestyle='--', alpha=0.6)
+  plt.show()
+
